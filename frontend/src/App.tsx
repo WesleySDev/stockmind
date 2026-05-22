059074f6737366d423dc974135a40c07b8b6e1ab
WesleySDev <you@example.com>
2026-05-21 22:03:24 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-zinc-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        StockMind 
+      </h1>
+    </div>
+  )
+}
