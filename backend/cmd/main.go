beffcf7b065d275458458b97c4b76fa512748f6c
WesleySDev <you@example.com>
2026-06-04 21:36:12 -0300

    fix: Corrigindo variáveis de ambiente e ajustando a conexão com o banco de dados

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"stockmind/database"
-	"stonkmind/database"
 
 	"github.com/gofiber/fiber/v2"
 )
--- backend/database/database.go
+++ backend/database/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 func Connect() {
 
-	dsn := "host=localhost user=postgres password=123456 dbname=stonckmind port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=gaza dbname=stockmind port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
